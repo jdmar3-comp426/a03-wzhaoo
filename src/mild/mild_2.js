ab3a52114ffe7455dad334371e016cdea0783b25
wzhaoo <willxiaozhao@gmail.com>
2021-11-20 17:36:01 -0500

    mild2

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -48,7 +48,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-   delete object.key;
+   delete object[key];
 }
 
 /**
