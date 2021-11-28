c3df3cf0bd9ffe2111f20feeab91b93fde6e9c14
wzhaoo <willxiaozhao@gmail.com>
2021-11-27 20:06:54 -0500

    ?

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -23,7 +23,8 @@ export function getSum(array) {
  */
 export function getMedian(array) {
     array.sort();
-    if(array.length%2==0) return (array[(array.length-1)/2]+array[array.length/2])/2
+    console.log(array);
+    if(array.length%2==0) return (array[array.length/2-1]+array[array.length/2])/2
     return array[array.length/2];
 }
 
